47a5d5ae38529b1fd868e2cc2528a564b7e4b44e
qspencer0224 <quentonspencer4@gmail.com>
2023-09-02 22:00:26 -0500

    logged in

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { getUser } from './utilities/users-service';
+
+// PAGES
+import AuthPage from './pages/AuthPage/AuthPage';
+import Home from './pages/Home/Home';
+import NewNote from './pages/NewNote/NewNote';
+import OrderHistoryPage from './pages/OrderHistoryPage/OrderHistoryPage';
+//------------
+
+// components
+import NavBar from './components/NavBar/NavBar';
+// css
+import './App.css';
+
+function App() {
+  // array destructuring
+  const [user, setUser] = useState(getUser());
+
+  return (
+    <main className='App'>
+      {user ? (
+        <>
+          <NavBar user={user} setUser={setUser} />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/orders/new' element={<NewNote />} />
+            <Route path='/orders' element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+      ) : (
+        <AuthPage setUser={setUser} />
+      )}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/NewNote/NewNote.jsx
@@ -0,0 +1,5 @@
+export default function NewNote() {
+    return (
+        <h1>New Note</h1>
+    )
+}
