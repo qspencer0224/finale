6cfe7d6e37f4577c14896111d930736a6bd4ac9a
qspencer0224 <quentonspencer4@gmail.com>
2023-09-05 21:15:16 -0500

    more

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,24 +1,30 @@
-import axios from 'axios'
+
 import React from 'react'
 import { useState } from 'react'
 
 
 function Form({active, setActive}) { 
-  const [edit, setEdit] = useState("")
+  const [edit, setEdit] = useState(active)
   const handleChange = (event) =>{
-    //this function is setting whatever we put into our form as a post 
-    // setNewTask({post: event.target.value})
+    // Update the edit state with the new value from the input
+    setEdit(event.target.value)
   }
 
   const updateEntry = async () =>{
-    
+    setActive({ ...active, post: edit})
   }
   return (
     <div>
-      <form>
-        {/* <input onChange={handleChange} type='text' name='post' value={active.post} /> */}
+      <form onSubmit={updateEntry}>
+        <input 
+        onChange={handleChange} 
+        type='text' 
+        name='post' 
+        value={edit} />
         <br />
-        <input type='submit'/>
+        <input 
+        type='submit' 
+        value='Submit'/>
       </form>
     </div>
   )
